f1eb542bb3f1c8aa24e944fc49ce8f28562ff8a1
monochromegane <dev.kuro.obi@gmail.com>
2015-09-23 20:52:47 +0900

    Initial commit.

---
+++ client.go
@@ -0,0 +1,34 @@
+package slack_incoming_webhooks
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type Client struct {
+	WebhookURL string
+}
+
+func (c Client) Post(p Payload) error {
+
+	json, err := json.Marshal(p)
+	if err != nil {
+		return err
+	}
+
+	resp, err := http.PostForm(c.WebhookURL, url.Values{"payload": []string{string(json)}})
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		return fmt.Errorf("%d: %s", resp.StatusCode, body)
+
+	}
+	return nil
+}
---
+++ payload.go
@@ -0,0 +1,9 @@
+package slack_incoming_webhooks
+
+type Payload struct {
+	Channel   string `json:"channel,omitempty"`
+	Username  string `json:"username,omitempty"`
+	Text      string `json:"text,omitempty"`
+	IconEmoji string `json:"icon_emoji,omitempty"`
+	IconURL   string `json:"icon_url,omitempty"`
+}
